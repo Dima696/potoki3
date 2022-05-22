ddca54a05fb34bb8ee2f94c02db7ed620b6e6db7
Dima696 <100032055+Dima696@users.noreply.github.com>
2022-05-22 18:36:44 +0300

    Update Main.java
    Исправление

--- src/Main.java
+++ src/Main.java
@@ -8,23 +8,24 @@ public class Main {
         GameProgress gameProgress2 = new GameProgress(10, 20, 30, 2.0);
         GameProgress gameProgress3 = new GameProgress(20, 30, 40, 3.0);
 
-
         new File("Games").mkdir();
         new File("Games", "savegames").mkdir();
 
-        gameProgress1.saveGame(gameProgress1, "C:\\Games\\savegames\\save1.dat");
-        gameProgress2.saveGame(gameProgress2, "C:\\Games\\savegames\\save2.dat");
-        gameProgress3.saveGame(gameProgress3, "C:\\Games\\savegames\\save3.dat");
+        gameProgress1.saveGame(gameProgress1, "Games" + File.separator + "savegames" + File.separator + "save1.dat");
+        gameProgress2.saveGame(gameProgress2, "Games" + File.separator + "savegames" + File.separator + "save2.dat");
+        gameProgress3.saveGame(gameProgress3, "Games" + File.separator + "savegames" + File.separator + "save3.dat");
 
 
-        gameProgress1.zipFiles("C:\\Games\\savegames\\zip.zip", "C:\\Games\\savegames\\save1.dat");
-        gameProgress1.zipFiles("C:\\Games\\savegames\\zip.zip", "C:\\Games\\savegames\\save2.dat");
-        gameProgress1.zipFiles("C:\\Games\\savegames\\zip.zip", "C:\\Games\\savegames\\save3.dat");
-
+        gameProgress1.zipFiles("Games" + File.separator + "savegames" + File.separator + "zip.zip",
+                "Games" + File.separator + "savegames" + File.separator + "save1.dat");
+        gameProgress1.zipFiles("Games" + File.separator + "savegames" + File.separator + "zip.zip",
+                "Games" + File.separator + "savegames" + File.separator + "save2.dat");
+        gameProgress1.zipFiles("Games" + File.separator + "savegames" + File.separator + "zip.zip",
+                "Games" + File.separator + "savegames" + File.separator + "save3.dat");
         try {
-            File file1 = new File("C:\\Games\\savegames\\save1.dat");
-            File file2 = new File("C:\\Games\\savegames\\save2.dat");
-            File file3 = new File("C:\\Games\\savegames\\save3.dat");
+            File file1 = new File("Games" + File.separator + "savegames" + File.separator + "save1.dat");
+            File file2 = new File("Games" + File.separator + "savegames" + File.separator + "save2.dat");
+            File file3 = new File("Games" + File.separator + "savegames" + File.separator + "save3.dat");
             if (file1.exists() && file2.exists() && file3.exists()) {
                 file1.delete();
                 file2.delete();
@@ -33,7 +34,5 @@ public class Main {
         } catch (Exception ex) {
             System.err.println(ex);
         }
-
     }
-
 }
